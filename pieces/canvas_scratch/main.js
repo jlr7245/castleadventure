4ede2dcce39d8666364e45d6436947a44d57f5c6
J Silverstein <jess.pebble@gmail.com>
2016-12-14 22:25:30 -0500

    got drawing to work

--- pieces/canvas_scratch/main.js
+++ pieces/canvas_scratch/main.js
@@ -25,14 +25,14 @@ const courtyardWall = {
   fixedLocation: true,
 };
 
-/// 780 x 590 // 1280 x 800
+/// 780 x 590 // 1280 x 800 // x 330, y 235, w 120, h 220
 
 class ExitPoint {
-  constructor(x, y, w, h) {
-    this.x = x || 330;
-    this.y = y || 235;
-    this.w = w || 120;
-    this.h = h || 220;
+  constructor(arr) {
+    this.x = arr[0];
+    this.y = arr[1];
+    this.w = arr[2];
+    this.h = arr[3];
     this.fill = '#010101';
   }
   
@@ -52,17 +52,19 @@ let castleCourtyard = {
   lookableAttributes: [gate, courtyardWall],
   userPositionX: 460,
   userPositionY: 460,
-  entryAndExit: [(330, 0, 120, 0)],
+  entryAndExit: [ [330, 0, 120, 5], [0, 235, 5, 120] ],
+  //wallStyle: square,
 };
 
 class Room {
-  constructor(obj) {
+  constructor(canv, obj) {
+    for (let key in canv) { this[key] = canv[key]; }
     for (let fld in obj) { this[fld] = obj[fld]; } // looping through object passed
     this.entryAndExitArr = [];
     this.setEntryAndExit();
     this.drawExit();
     this.drawWalls();
-    this.drawUser(this.userPositionX, this.userPositionY);
+    this.drawUser(this.userPositionX, this.userPositionY, this.ctx);
   } /// end of constructor
   
   setEntryAndExit() {
@@ -83,34 +85,34 @@ class Room {
     console.log('testing');
   }
   
-  drawUser(x, y) {
-    console.log(x, y);
+  drawUser(x, y, ctx) {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(x,y,20,20);
   }
   
 }
 
 class CanvasState {
-  constructor(arg, canvas) {
+  constructor(canvas) {
     this.canvas = canvas;
     this.width = canvas.width;
     this.height = canvas.height;
     this.ctx = canvas.getContext('2d');
-    for (let key in arg) { this[key] = arg[key]; }
 
   }
   //// stuff goes here
 }
 
 var canv;
-var test;
-var current;
+var theRoom;
+var canvas;
 
-let init = function init() {
+let init = function init(arg) {
   canv = document.getElementById('canvas');
-  current = new Room(castleCourtyard); ///// got a scope problem in here... tries to draw the exit point before there's a context.... i guess i have to create a canvas state first and then i can create a room with the canvas state??????
-  test = new CanvasState(current, canv);
+  canvas = new CanvasState(canv); 
+  theRoom = new Room(canvas, arg);
 };
 
 window.onload = function() {
-  init();
+  init(castleCourtyard);
 };
